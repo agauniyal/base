ed51f1524ca7a3b82be1f152eb5937e11c7d5d10
agauniyal <agauniyal@outlook.com>
2017-02-04 23:48:29 +0530

    Increase few basic tests

--- test/selection_sort_test.cpp
+++ test/selection_sort_test.cpp
@@ -8,7 +8,7 @@ TEST(selection_sort, ascending)
 	std::random_device rnd_device;
 	std::mt19937 m_engine(rnd_device());
 	std::uniform_int_distribution<int> dist(1, 100000);
-	auto gen = std::bind(dist, m_engine);
+	const auto gen = std::bind(dist, m_engine);
 
 	std::vector<int> v1(1000);
 	std::generate(v1.begin(), v1.end(), gen);
@@ -26,16 +26,37 @@ TEST(selection_sort, descending)
 	std::random_device rnd_device;
 	std::mt19937 m_engine(rnd_device());
 	std::uniform_int_distribution<int> dist(1, 52);
-	auto gen = std::bind(dist, m_engine);
+	const auto gen = std::bind(dist, m_engine);
 
 	std::vector<int> v1(1000);
 	std::generate(v1.begin(), v1.end(), gen);
 	auto v2 = v1;
 
-	auto desc = [](auto a, auto b) { return a > b; };
+	const auto desc = [](auto a, auto b) { return a > b; };
 
 	base::selection::sort(v1.begin(), v1.end(), desc);
 	std::sort(v2.begin(), v2.end(), desc);
 
 	ASSERT_EQ(v1, v2);
+}
+
+
+TEST(selection_sort, noElement)
+{
+	std::vector<int> v1;
+	const auto desc = [](auto a, auto b) { return a > b; };
+	base::selection::sort(v1.begin(), v1.end(), desc);
+
+	EXPECT_EQ(0, v1.size());
+}
+
+
+TEST(selection_sort, singleElement)
+{
+	std::vector<int> v1{ 0 };
+	const auto v2   = v1;
+	const auto desc = [](auto a, auto b) { return a > b; };
+	base::selection::sort(v1.begin(), v1.end(), desc);
+
+	ASSERT_EQ(v2, v1);
 }
